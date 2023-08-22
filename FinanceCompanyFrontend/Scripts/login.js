a471c21aa3315a0ea97f061ae5cf176a8fe941c6
mouryacheggoju <saimouryacheggoju456@gmail.com>
2023-08-22 14:28:22 +0530

    commit

--- FinanceCompanyFrontend/Scripts/login.js
+++ FinanceCompanyFrontend/Scripts/login.js
@@ -1,15 +1,16 @@
-async function fetchData(data) {
+async function fetchData(data, role) {
     try {
         const headers = new Headers();
-        headers.append("Content-Type", "application/json"); 
-        
+        headers.append("Content-Type", "application/json");
+        headers.append("Role",{1:"MANAGER",2:"CLERK",3:"CUSTOMER"}[role])
+        console.log("this: "+{1:"MANAGER",2:"CLERK",3:"CUSTOMER"}[role])
         const requestOptions = {
             method: 'POST',
             headers: headers,
             body: JSON.stringify(data)
         };
-
         const response = await fetch('http://localhost:8080/FinanceCompanyBackend/rest/auth/login', requestOptions);
+        console.log("after fetch")
         if (!response.ok) {
             throw new Error('Request failed');
         }
@@ -20,31 +21,24 @@ async function fetchData(data) {
         throw error;
     }
 }
-
-
 async function upload_data() {
     const email = document.getElementById("email").value;
     const role = document.getElementById("role").value;
     const password = document.getElementById("password").value;
-
     var data = {
         "username": email,
         "password": password,
-        "role": role
     }
-
-    var post_response = await fetchData(data)
+    var post_response = await fetchData(data, role)
     if(post_response.statusCode == 200){
         alert("Welcome!")
         customer_id = post_response.responseData.customerId
-        role_id = post_response.responseData.role
-
+        // role_id = post_response.responseData.role
         localStorage.clear()
         localStorage.setItem('customer_id', customer_id)
-
-        switch (role_id) {
+        switch (role) {
             case 1:
-                location.replace("Pages/Manager/manager_home.html")
+                location.replaace("Pages/Manager/manager_home.html")
                 break;
             case 2:
                 location.replace("Pages/Clerk/clerk_home.html")
@@ -53,13 +47,10 @@ async function upload_data() {
                 location.replace("Pages/Customer/customer_home.html")
                 break;
         }
-
     }else{
         alert("InValid Username or Password")
     }
 }
-    
-
 window.addEventListener('DOMContentLoaded',()=>{
     document.getElementById("loginButton").addEventListener('click',async ()=>{
         upload_data()
